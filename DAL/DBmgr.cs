4dde2e4c990c64cbd4db05d8cb5c210ce42c8311
Omkar Yadav <130984348+omkaryadav4433@users.noreply.github.com>
2023-07-24 20:55:59 +0530

    Add files via upload

---
+++ DAL/DBmgr.cs
@@ -0,0 +1,65 @@
+﻿using BOL;
+using MySql.Data.MySqlClient;
+using System.Linq.Expressions;
+
+namespace DAL
+
+{
+    public class DBmgr
+    {
+        public static string costring = @"server=localhost;port=3306;user=root;password=root123;database=yadav";
+        public static List<student> getallstudents()
+        {
+            List<student> std = new List<student>();
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = costring;
+            try { conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                string query = "select * from student";
+                cmd.CommandText = query;
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    /* private string id;
+                     private string name;
+                     private int age;
+                     public DateOnly bdate;
+                     public string course;
+                     private double fees;*/
+
+                    string id = reader["id"].ToString();
+                    string name = reader["name"].ToString();
+                    int age = int.Parse(reader["age"].ToString());
+                    string course = reader["course"].ToString();
+                    double fees = double.Parse(reader["fees"].ToString());
+                    DateOnly bdate = DateOnly.Parse(reader["bDate"].ToString().Substring(0, 10));
+
+                    student student = new student
+                    {
+                        Id = id,
+                        Name = name,
+                        Age = age,
+                        bdate = bdate,
+                        Course = course,
+                        Fees = fees,
+                    };
+
+                    std.Add(student);
+                }
+                
+            }
+        catch(Exception exception)
+        {
+
+            Console.WriteLine(exception.Message);
+
+        }
+        finally{conn.Close();}
+
+    return std;
+        }
+}
+
+}
---
+++ studentmanagement/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using DAL;
+using BOL;
+
+
+List<student> student = DBmgr.getallstudents();
+
+foreach(student stud in student)
+{
+    Console.WriteLine(stud.Name+" "+stud.bdate);
+}
+
+
+
+
